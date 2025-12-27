2a70a7c4a73c5564bc908161a198e254051e978c
shivanshsw <173685371+shivanshsw@users.noreply.github.com>
2025-12-27 14:22:28 +0530

    upload test

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI ,File ,UploadFile
+from typing import Annotated, Optional
 app = FastAPI()
 
 #jai ganesh🙏
+
+
+
 @app.get("/")
 def happy():
     return {"new":"done"}
 @app.get("/health")
 def health():
     return {"status":"ok"}
+
+
+#this is supposed to be the file upload part before unstructured
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...)):
+    return {"filename":file.filename,"type":file.content_type}
+
+
